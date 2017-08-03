72737afc9792c310abb5280b2296ca827b116c9e
Daniel Bogusz <respown@gmail.com>
2017-08-04 01:32:11 +0200

    removed <script> tags from .js file (they shouldn't be there)

--- js/scrip.js
+++ js/scrip.js
@@ -1,24 +1,30 @@
-   <script type="text/javascript">
 
-        	var numer = Math.floor(Math.random()*3)+1;
-        
-            function hide()
-            {
-               $("#slide").fadeOut(500); 
-            }
-		
-			function zmienslajd()
-			{
-				numer++; if (numer>3) numer=1;
-				
-				var plik = "<img src=\"pic/slide" + numer + ".jpg\" />";
-				
-				document.getElementById("slide").innerHTML = plik;
-			
-                $("#slide").fadeIn(400);
-                
-				setTimeout("zmienslajd()",5000);
-                setTimeout("hide()",4600);	
-            }
+
+var numer = Math.floor(Math.random()*3)+1;
+
+function hide()
+{
+   $("#slide").fadeOut(500); 
+}
+
+
+/**
+* Poszukaj czegoś co spowoduje zatrzymanie domyślnej akcji po kliknięciu na strzałki do zmiany slajdu
+* to będzie: preventDefault, składnia w stylu: $(element).on('click', funkcja) i wtedy wywołanie tej funkcji zmienslajd (wywołujesz bez nawiasów żeby przypisać funcje a nie spowodować jej natychmiastowe wywołanie i przypisać wartość którą to wywołanie spowoduje
+* 
+*/
+
+function zmienslajd()
+{
+	numer++; if (numer>3) numer=1;
+	
+	var plik = "<img src=\"pic/slide" + numer + ".jpg\" />";
+	
+	document.getElementById("slide").innerHTML = plik;
+
+	$("#slide").fadeIn(400);
+	
+	setTimeout("zmienslajd()",5000);
+	setTimeout("hide()",4600);	
+}
   
-      </script>
